da85add0f40ab527c62aec1d94a79eb986071b19
Daniel Friedman <friedmandaniel111@gmail.com>
2025-02-12 12:47:17 -0800

    v1.0

---
+++ README.md
@@ -0,0 +1,106 @@
+### Version 1.0
+
+# Features
+
+After installing, the bot will automatically monitor the heartbeat channel and determine which discord members are actively rerolling or not. Then, it will assign roles and update an `ids.txt` file on github, which is referenced by all AHK Farmers in the server! This allows for seamless joining/leaving sessions with no interaction apart from starting / stopping the ahk script.
+
+# Usage Instructions
+
+## AHK Setup
+
+- Friend ID: `https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/refs/heads/main/ids.txt`
+
+- Discord ID: *User's discord ID*
+
+- Discord Webhook URL: *Webhook created in Pack Alert channel*
+
+- Discord Heartbeat: ✅
+    - Box 1: *User's discord ID*
+    - Box 2: *Webhook created in Heartbeat channel*
+
+## Commands
+
+- **/profile set `friend_code` `instances`**
+    - Connect PTCGP friend code to discord ID
+    - Instances field currently has no effect
+
+- **/profile get `discord_id`**
+    - Display information about user such as activity status, last active date, and total time active
+
+- **/profile manage `discord_id` `param` `value`** *(Admin only)*
+    - Edit a user's friend code, activity status, or time active
+    - Value must be `true` or `false` for activity status
+
+- **/usernames**
+    - Generates a file with 200 numbered usernames to use in your ahk folder for better alt identification
+
+- **/offline**
+    - Manual switch to turn your activity off, since bot can only update ~30min after last heartbeat
+
+- **/gp_status `status`**
+    - Used in pack thread - changes thread tag to dead or alive
+
+# Installation Instructions
+
+## Prerequisites
+- Python 3.8 or higher
+- pip (Python package installer)
+- Git
+- Discord server with:
+    - Separate channels for both webhooks (Heartbeat, Packs)
+    - Forum channel (For threads)
+        - Three tags in said channel (alive, dead, and unknown)
+    - Active and Inactive user roles
+
+## Step 1: Clone the Repository
+```bash
+git clone https://github.com/Dan1elTheMan1el/tcgpbot.git
+```
+
+## Step 2: Create a GitHub Repository / Token
+1. Go to [GitHub](https://github.com) and log in to your account.
+2. Click on the "+" icon in the top right corner and select "New repository".
+3. Name your repository and click "Create repository".
+4. In the root directory of this repository, create a file named `ids.txt` (Leave empty).
+5. Create a GitHub fine-grained personal access token with `repo` scope by following [these instructions](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token).
+6. Under the "TOKEN" section, click "Copy" to copy your bot token. You will need these details for the `.env` file.
+
+## Step 3: Create a Discord Application
+1. Go to the [Discord Developer Portal](https://discord.com/developers/applications).
+2. Click on "New Application".
+3. Name your application and click "Create".
+4. Navigate to the "Bot" tab on the left sidebar.
+5. Click "Add Bot" and confirm by clicking "Yes, do it!".
+6. Under the "TOKEN" section, click "Copy" to copy your bot token. You will need this for the `.env` file.
+
+## Step 4: Create the `.env` File
+1. In the root directory of the cloned repository, create a file named `.env`.
+2. Open the `.env` file and add the following lines (REPLACE FIELDS):
+    ```
+    DISCORD_TOKEN=discord_bot_token
+    GUILD=discord_server_id
+    HEARTBEAT=heartbeat_channel_id
+    PACKALERT=webhook_channel_id
+
+    PACKFORUM=pack_forum_channel_id
+    PACKTAG=unknown_pack_tag_id
+    PACKLIVETAG=live_pack_tag_id
+    PACKDEADTAG=dead_pack_tag_id
+
+    GITHUB_TOKEN=github_token
+    GITHUB_USER=your_username
+    GITHUB_REPO=your_repo
+    GITHUB_FILEPATH=ids.txt
+    ```
+
+## Step 5: Install Dependencies
+- py-cord (v2.0+, see documentation for install)
+- python-dotenv
+- requests
+
+## Step 6: Run the Bot
+```bash
+python wpgp-bot.py
+```
+
+Your bot should now be running and connected to your Discord server!
---
+++ wpgp-bot.py
@@ -0,0 +1,292 @@
+import os
+import discord
+import json
+import datetime
+import requests
+import base64
+from discord.ext import tasks
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+# Discord related
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('GUILD')
+HEARTBEAT = os.getenv('HEARTBEAT')
+PACKALERT = os.getenv('PACKALERT')
+# Pack forum related
+PACKFORUM = os.getenv('PACKFORUM')
+PACKTAG = os.getenv('PACKTAG')
+PACKLIVETAG = os.getenv('PACKLIVETAG')
+PACKDEADTAG = os.getenv('PACKDEADTAG')
+
+# GitHub related
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+GITHUB_USER = os.getenv('GITHUB_USER')
+GITHUB_REPO = os.getenv('GITHUB_REPO')
+GITHUB_FILEPATH = os.getenv('GITHUB_FILEPATH')
+
+# Commit to GitHub
+async def update_github():
+    # Get all active friend codes
+    fcs = []
+    for user in data:
+        if data[user]['status']:
+            fcs.append(data[user]['fc'])
+    text = '\r\n'.join(fcs)
+
+    # Get the sha of the ids.txt file
+    url = f"https://api.github.com/repos/{GITHUB_USER}/{GITHUB_REPO}/contents/{GITHUB_FILEPATH}"
+    headers = { 'Authorization': f'token {GITHUB_TOKEN}' }
+    response = requests.get(url, headers=headers)
+    sha = response.json()['sha']
+
+    # Update the ids.txt file
+    commitData = {
+        'message': 'Updated ids.txt from Discord',
+        'content': base64.b64encode(text.encode()).decode(),
+        'sha': sha
+    }
+    response = requests.put(url, headers=headers, json=commitData)
+    print('Updated GitHub!')
+
+# Update internal friend code status
+async def update_fcids(guild):
+    roleActive = discord.utils.get(guild.roles, name="Active")
+    roleInactive = discord.utils.get(guild.roles, name="Inactive")
+    for user in data:
+        member = guild.get_member(int(user))
+        if data[user]['status']:
+            # Check if last heartbeat was too long ago
+            now = datetime.datetime.now()
+            last_on = datetime.datetime.strptime(data[user]['last_on'], "%Y-%m-%d %H:%M:%S.%f")
+            if (now - last_on).total_seconds() > 60 * 33: # 33 minutes for extra buffer
+                data[user]['status'] = False
+                await member.add_roles(roleInactive)
+                await member.remove_roles(roleActive)
+                print(f"{member.display_name} is offline!")
+            else:
+                await member.add_roles(roleActive)
+                await member.remove_roles(roleInactive)
+        else:
+            await member.add_roles(roleInactive)
+            await member.remove_roles(roleActive)
+    
+    # Update logged data
+    json_file = open('data.json', 'w')
+    json.dump(data, json_file)
+    json_file.close()
+
+# Bot stuff
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = discord.Bot(intents=intents)
+
+# Initialize data
+json_file = open('data.json')
+data = json.load(json_file)
+json_file.close()
+
+profile = bot.create_group("profile", "Manage Profile")
+
+# Command: Link friend code to user's Discord ID
+@profile.command()
+async def set(ctx, fc: str, instances: int):
+    if len(fc) != 16:
+        await ctx.respond("Please enter your PTCGP friend code without hyphens.")
+    else:
+        author_id_str = str(ctx.author.id)
+        data[author_id_str] = {
+            'status': False,
+            'fc': fc,
+            'instances': instances,
+        }
+
+        # Update logged data
+        json_file = open('data.json', 'w')
+        json.dump(data, json_file)
+        json_file.close()
+
+        await ctx.respond(f"Friend code set to {fc}!")
+        await update_fcids(ctx.guild)
+
+# Command: Get user's profile / information
+@profile.command()
+async def get(ctx, discord_id: str):
+    if discord_id in data:
+        if 'last_on' in data[discord_id]:
+            now = datetime.datetime.now()
+            last_on = datetime.datetime.strptime(data[discord_id]['last_on'], "%Y-%m-%d %H:%M:%S.%f")
+            days_ago = (now - last_on).days
+            hours_ago = round(((now - last_on).seconds / 3600)%24, 1)
+        else:
+            days_ago = 'N/A'
+            hours_ago = 'N/A'
+
+        status = 'Online' if data[discord_id]['status'] else f'Offline, last seen {days_ago} days and {hours_ago} hours ago'
+        name = await bot.fetch_user(int(discord_id))
+        
+        hours = data[discord_id]['hours'] if 'hours' in data[discord_id] else 0
+        await ctx.respond(f"**{name}**\nFriend code: {data[discord_id]['fc']}\nStatus: {status}\nHours farmed: {hours}\nInstances: {data[discord_id]['instances']}")
+    else:
+        await ctx.respond("User not found.")
+
+# Admin command: Edit user's profile
+@profile.command()
+@discord.option("param",choices=["fc","status","hours",])
+async def manage(ctx, discord_id: str, param: str, value: str):
+    if not ctx.author.guild_permissions.administrator:
+        await ctx.respond("You do not have permission to use this command.", ephemeral=True)
+        return
+
+    if discord_id not in data:
+        await ctx.respond("User not found.", ephemeral=True)
+        return
+    
+    value_update = value if param == "fc" else (round(float(value)*2,0)/2 if param == "hours" else (True if value == "true" else False))
+    data[discord_id][param] = value_update
+    if param == "status" and value_update:
+        data[discord_id]['last_on'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+    json_file = open('data.json', 'w')
+    json.dump(data, json_file)
+    json_file.close()
+
+    await ctx.respond(f"✅ {param} updated!")
+    await update_fcids(ctx.guild)
+
+# Command: Generate usernames.txt for self
+@bot.command()
+async def usernames(ctx):
+    author = ctx.author.display_name
+    if len(author) > 11:
+        author = author[0:11]
+    usernames = []
+    for num in range(1, 201):
+        usernames.append(f"{author}{num}")
+    
+    user_file = open('usernames.txt', 'w')
+    user_file.write('\n'.join(usernames))
+    user_file.close()
+    discord_file = discord.File('usernames.txt')
+    await ctx.respond(file=discord_file)
+
+# Command: Update god pack thread status
+@bot.command()
+@discord.option("status",choices=["Live","Dead"])
+async def gp_status(ctx, status: str):
+    if not str(ctx.channel.type) == "public_thread":
+        await ctx.respond(f"This command can only be used in a pack thread.", ephemeral=True)
+        return
+    
+    pack_forum = ctx.guild.get_channel(int(PACKFORUM))
+    tag = pack_forum.get_tag(int(PACKLIVETAG)) if status == "Live" else pack_forum.get_tag(int(PACKDEADTAG))
+    await ctx.channel.edit(applied_tags=[tag])
+    await ctx.respond("✅ Status updated!")
+
+# Command: Set own status to offline manually
+@bot.command()
+async def offline(ctx):
+    if str(ctx.author.id) not in data:
+        await ctx.respond("Please set your friend code first.", ephemeral=True)
+        return
+    
+    data[str(ctx.author.id)]['status'] = False
+
+    json_file = open('data.json', 'w')
+    json.dump(data, json_file)
+    json_file.close()
+
+    await update_fcids(ctx.guild)
+    await ctx.respond("✅ Status set to offline!")
+
+# Admin command: Get full user data (for debug)
+@bot.command()
+async def get_json(ctx, discord_id: str):
+    if not ctx.author.guild_permissions.administrator:
+        await ctx.respond("You do not have permission to use this command.", ephemeral=True)
+        return
+    
+    if discord_id in data:
+        await ctx.respond(data[discord_id])
+    else:
+        await ctx.respond("User not found.")
+
+# Events for reading messages
+@bot.event
+async def on_message(message):
+    # Ignore messages from the bot itself
+    if message.author == bot.user:
+        return
+    
+    # Heartbeat channel
+    if message.channel.id == int(HEARTBEAT):
+        # Ensure message is from webhook
+        if message.webhook_id:
+            lines = message.content.split('\n')
+            online_id = lines[0]
+
+            # Ensure user has set their friend code
+            if online_id not in data:
+                await message.add_reaction("❓")
+                await update_fcids(message.guild)
+                return
+            
+            # Update user status / profile
+            data[online_id]['status'] = True
+            if 'hours' in data[online_id]:
+                now = datetime.datetime.now()
+                last_on = datetime.datetime.strptime(data[online_id]['last_on'], "%Y-%m-%d %H:%M:%S.%f")
+                if (now - last_on).total_seconds() > 60 * 29:
+                    data[online_id]['hours'] = data[online_id]['hours'] + 0.5
+            else:
+                data[online_id]['hours'] = 0.5
+            data[online_id]['last_on'] = message.created_at.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+            # Update logged data
+            json_file = open('data.json', 'w')
+            json.dump(data, json_file)
+            json_file.close()
+
+            print(f"{message.guild.get_member(int(online_id)).display_name} is online!")
+            await message.add_reaction("❤️")
+            await update_fcids(message.guild)
+
+    # Pack alert channel
+    elif message.channel.id == int(PACKALERT):
+        # Ensure message is from webhook
+        if message.webhook_id:
+            lines = message.content.split('\n')
+            # Ignore other alerts in the channel
+            if len(lines) < 3:
+                return
+            if lines[2].startswith("Found a God Pack"):
+                acc_name = lines[1].split('(')[0]
+                acc_fc = lines[1].split('(')[1][0:16]
+                packs_num = lines[2].split('(')[1][0]
+                file = message.attachments[0].url
+                roleActive = discord.utils.get(message.guild.roles, name="Active")
+                pack_forum = message.guild.get_channel(int(PACKFORUM))
+                tag = pack_forum.get_tag(int(PACKTAG))
+                packnumdata = [(1,1),(5,9),(8,15),(11,21)]
+                await pack_forum.create_thread(name=f"{acc_name} [{packs_num}P]", content=f"{roleActive.mention}\nClear your friends list and check your wonder pick!\nFriend code: `{acc_fc}`\nUse `/gp_status` to update thread tag\nOther packs needed for 95% confidence of dud: **{packnumdata[int(packs_num)-1][0]}**\nOther packs needed for 99.7% confidence of dud: **{packnumdata[int(packs_num)-1][1]}**\n{file}", applied_tags=[tag])
+
+# Event on bot startup
+@bot.event
+async def on_ready():
+    print('---------------------')
+    print(f'Started bot at {datetime.datetime.now()}')
+    print('---------------------\n')
+
+    # Begin auto update loop
+    auto_update.start()
+    guild = bot.get_guild(int(GUILD))
+    await update_fcids(guild)
+
+# Auto update loop
+@tasks.loop(minutes=2)
+async def auto_update():
+    await update_github()
+
+bot.run(DISCORD_TOKEN)
